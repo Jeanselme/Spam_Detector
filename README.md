55601ad98d197dd71c64a2c3a6420b85a4f2cda3
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2016-10-03 16:12:31 +0200

    Download the dataset

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Spam_Detector
 A simple example to apply machine learning algorithm to an everyday used algorithm
+
+## DataSet
+This algorithm uses the dataset extracted from the website of Lichman, M. (2013). UCI Machine Learning Repository [http://archive.ics.uci.edu/ml]. Irvine, CA: University of California, School of Information and Computer Science. 
---
+++ download.py
@@ -0,0 +1,32 @@
+import os
+import zipfile
+from urllib.request import urlretrieve
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/"
+file = "spambase.zip"
+
+def extract(fileName, destination) :
+	"""
+	Extract the content of the document
+	"""
+	if not(os.path.exists(destination)) :
+		with zipfile.ZipFile(fileName, "r") as zipFile:
+			print("Extraction")
+			zipFile.extractall(destination)
+			print("Extraction Complete")
+	else :
+		print("Destination directory already exists")
+
+def downloadAndExtractFile(destination, fileName = file, adress = url, force = False) :
+	"""
+	Downloads the file at the given adress and saves it under fileName
+	"""
+	if not(os.path.exists(fileName)) or force :
+		print("Download")
+		urlretrieve (adress + fileName, fileName)
+		print("Download Complete")
+		extract(fileName, destination)
+	else :
+		print("File already present on your system")
+
+downloadAndExtractFile("SpamDataSet")
